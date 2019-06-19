aaf9a25cfd397a5cce144989dd2aca2ab47986f7
kylewill0725 <kylewill0725@outlook.com>
2019-06-19 14:07:38 -0500

    Added Shell Component

---
+++ src/components/Shell.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>{{msg}}</p>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'Shell',
+    props: {
+        msg: String
+    }
+})
+</script>
